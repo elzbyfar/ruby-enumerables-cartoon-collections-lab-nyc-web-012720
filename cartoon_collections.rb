cb862281b0f0c0e48914a684e23bd880e7df3be5
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-21 22:38:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,11 +19,10 @@ end
 def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
   i = 0 
-  while i < cheese_types do 
-    foods.find(cheese_types[i])
+  while i < cheese_types.length do 
+    foods.find { |food| food == cheese_types[i] }
     i+=1 
   end 
-  
 end
 
 
