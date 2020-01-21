8a18b3ed2d304601b0765d1709da47417d912ae4
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-21 22:21:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves_array)
+  numberedwarves = dwarves_array.each_with_index { |dwarf, index| 
 end
 
 def summon_captain_planet# code an argument here
