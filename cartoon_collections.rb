479553d5957f0605495a485ccbd5978191870c5f
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-21 22:42:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,8 +20,9 @@ def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
   i = 0 
   while i < cheese_types.length do 
-    found = foods.find { |food| food == cheese_types[i] }
-    return found
+    if foods.include? { |food| food == cheese_types[i] }
+      return found
+    end 
     i+=1 
   end 
   nil
