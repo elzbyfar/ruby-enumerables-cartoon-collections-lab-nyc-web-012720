5055f61da41d4ae7c0ef1c9b03b1adca8c52eb3e
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-21 22:22:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarves_array)
   numberedwarves = dwarves_array.each_with_index do |dwarf, index|
-    puts "#{index} #{dwarf}"
+    puts "#{index + 1} #{dwarf}"
   end 
 end
 
